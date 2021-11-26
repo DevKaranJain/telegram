3892012a70bfffcb552c4b5de61e09308448df08
KaranJain007 <92155097+KaranJain007@users.noreply.github.com>
2021-11-26 20:53:01 +0530

    go

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -73,7 +73,7 @@ define([
 
         connection.trigger('updateButton', {
             button: 'next',
-            text: 'done',
+            text: 'next',
             visible: true
         });
 
