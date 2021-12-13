66b2b35a2d5680a96d4765ad775538f229197bf7
karan jain <karan@cyntexa.com>
2021-12-13 16:28:21 +0530

    go

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -145,7 +145,11 @@ define([
                 {
                         // console.log("in the step three ");  
                         // document.getElementById("test1").innerHTML =content;
-                      
+                        var bod = $('#richtextvalue').val();
+                        if (!bod){
+                            console.log("in the if ");
+                            document.getElementById("messageBody").innerHTML =bod;
+                        }
                     
 
                     save();
