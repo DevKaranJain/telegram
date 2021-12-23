6e68dd5479edb39f1824a2010422debc1a6bb37f
karan jain <karan@cyntexa.com>
2021-12-23 12:07:17 +0530

    uplode

--- routes/activity.js
+++ routes/activity.js
@@ -145,8 +145,9 @@ exports.execute = function (req, res) {
 
 						var data = `{
 						 "chat_id":to,
-					   	"photo":messageBody,
-					   	
+					   	"photo":"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+					   	,
+					   	"caption":"now that working "
 						}`;
 
 						xhr.send(data);
