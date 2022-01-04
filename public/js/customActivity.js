009f75b6185b58ab66ea5bbb5a4cd8ceaa9fb9f9
karan jain <karan@cyntexa.com>
2022-01-04 11:45:37 +0530

    uplode

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -122,17 +122,17 @@ define([
                     var asid = $('#accountSid').val(); 
                     console.log("body--------"+body);
                     console.log("image link----"+ asid);
-                        if(body==='' && asid === undefined){
+                     //   if(body==='' && asid === undefined){
                             // document.getElementById("messageBodyNull").innerHTML = messageBodyerrorSlds;
                                  console.log('body coming null '+ body);
                                  console.log('image linkk'+ asid);
-                                  connection.trigger('ready');
-                        }else {
+                                //  connection.trigger('ready');
+                       // }else {
                             console.log('coming to else part');
                           //  connection.trigger('nextStep');
                             save();
                             
-                        }
+                        //}
                         
                     
                     // if(body == "" )
